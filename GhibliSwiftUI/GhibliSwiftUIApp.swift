1397c9f397d4859e4149a4d2fbb15f8398d30f7d
leonardsangoroh <leonardsangoroh@gmail.com>
2026-06-03 13:36:12 +0100

    Initial Commit

---
+++ GhibliSwiftUI/GhibliSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUIApp.swift
+//  GhibliSwiftUI
+//
+//  Created by Lee Sangoroh on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct GhibliSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GhibliSwiftUITests/GhibliSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUITests.swift
+//  GhibliSwiftUITests
+//
+//  Created by Lee Sangoroh on 03/06/2026.
+//
+
+import Testing
+@testable import GhibliSwiftUI
+
+struct GhibliSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
