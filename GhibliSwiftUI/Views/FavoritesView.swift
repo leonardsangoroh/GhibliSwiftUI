6c1904bb33d040ef8e14d353fc901ab2f1afe27e
leonardsangoroh <leonardsangoroh@gmail.com>
2026-07-03 14:12:08 +0100

    feat(tabview): create tab view

---
+++ GhibliSwiftUI/Views/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  GhibliSwiftUI
+//
+//  Created by Lee Sangoroh on 02/07/2026.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text("Favorite Films")
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
